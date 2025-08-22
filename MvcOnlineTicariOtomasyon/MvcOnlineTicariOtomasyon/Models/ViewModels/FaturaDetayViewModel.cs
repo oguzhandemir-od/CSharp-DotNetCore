7bd3a662049f63fd5f6a88874766b7110cc61204
oguzhandemir-od <oguzhan.demir1995@yandex.com>
2025-08-22 12:34:21 +0300

    Bills and Sweet Alerts

---
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Models/ViewModels/FaturaDetayViewModel.cs
@@ -0,0 +1,10 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Models.ViewModels
+{
+    public class FaturaDetayViewModel
+    {
+        public Faturalar Fatura { get; set; }
+        public IEnumerable<FaturaKalem> FaturaKalems { get; set; }
+    }
+}
