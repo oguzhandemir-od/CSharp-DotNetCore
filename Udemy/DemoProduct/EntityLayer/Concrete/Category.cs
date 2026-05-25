3557fd8dfa0cf8d9ac19cf10caeb26af5908f681
oguzhandemir-od <oguzhan.demir1995@yandex.com>
2026-05-25 12:17:25 +0300

    Entity Layer

---
+++ Udemy/DemoProduct/EntityLayer/Concrete/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
