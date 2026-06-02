af1fce51054e670c52025519e8919a07df36d0e9
oguzhandemir-od <oguzhan.demir1995@yandex.com>
2026-06-02 11:35:23 +0300

    Password Interface

---
+++ Playground/ECommerceSystem/ECommerceSystem.Application/Interfaces/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using ECommerceSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceSystem.Application.Interfaces
+{
+    public interface IAuthService
+    {
+        void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
+        bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt);
+        string CreateToken(User user);
+    }
+}
