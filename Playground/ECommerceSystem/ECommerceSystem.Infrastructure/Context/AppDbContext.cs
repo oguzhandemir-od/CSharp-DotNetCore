b4f55f9ee321b9be1f1170af6dbae9dfc4beea41
oguzhandemir-od <oguzhan.demir1995@yandex.com>
2026-06-02 11:23:58 +0300

    Context

---
+++ Playground/ECommerceSystem/ECommerceSystem.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ECommerceSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceSystem.Infrastructure.Context
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
