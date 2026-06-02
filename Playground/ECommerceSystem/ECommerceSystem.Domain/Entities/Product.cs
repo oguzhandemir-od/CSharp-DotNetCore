b8f35b5340259d4b4c675bab3252453762f74340
oguzhandemir-od <oguzhan.demir1995@yandex.com>
2026-06-02 10:33:55 +0300

    Entities

---
+++ Playground/ECommerceSystem/ECommerceSystem.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceSystem.Domain.Entities
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock {  get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ Playground/ECommerceSystem/ECommerceSystem.Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using ECommerceSystem.Domain.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceSystem.Domain.Entities
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+        public RoleEnum Role { get; set; }
+    }
+}
