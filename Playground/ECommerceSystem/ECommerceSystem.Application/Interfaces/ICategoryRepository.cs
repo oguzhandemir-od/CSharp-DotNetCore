6e2763e5a21b21ee259c09239d5d1ba451c4802b
oguzhandemir-od <oguzhan.demir1995@yandex.com>
2026-06-02 11:03:13 +0300

    Interfaces

---
+++ Playground/ECommerceSystem/ECommerceSystem.Application/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using ECommerceSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceSystem.Application.Interfaces
+{
+    internal interface ICategoryRepository : IGenericRepository<Category>
+    {
+        
+    }
+}
---
+++ Playground/ECommerceSystem/ECommerceSystem.Application/Interfaces/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceSystem.Application.Interfaces
+{
+    internal interface IGenericRepository<T>
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddEntityAsync(T entity);
+        Task UpdateEntityAsync(T entity);
+        Task DeleteEntityAsync(int id);
+    }
+}
