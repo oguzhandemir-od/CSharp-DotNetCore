d1ed1ff17d4536b94ad130552a9914424edf2f18
oguzhandemir-od <oguzhan.demir1995@yandex.com>
2026-06-20 23:09:15 +0300

    feat(webapi): integrate ILogger into exception handling middleware for non-blocking error tracking

--- Playground/GlobalPublishing.Ecosystem/GlobalPublishing.WebAPI/Middlewares/ExceptionHandlingMiddleware.cs
+++ Playground/GlobalPublishing.Ecosystem/GlobalPublishing.WebAPI/Middlewares/ExceptionHandlingMiddleware.cs
@@ -6,10 +6,12 @@ namespace GlobalPublishing.WebAPI.Middlewares
     public class ExceptionHandlingMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
 
-        public ExceptionHandlingMiddleware(RequestDelegate next)
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
         {
             _next = next;
+            _logger = logger;
         }
 
         public async Task InvokeAsync(HttpContext context)
@@ -20,11 +22,12 @@ namespace GlobalPublishing.WebAPI.Middlewares
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex,"Uygulamada hata meydana geldi! Hata Mesajı: {Message}",ex.Message)
                 await HandleExceptionAsync(context, ex);
             }
         }
 
-        private static Task HandleExcetionAsync(HttpContext context, Exception exception)
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
         {
             var statusCode = HttpStatusCode.InternalServerError;
             var message = "Sistemde beklenmeyen bir hata oluştu.";
